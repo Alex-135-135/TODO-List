277f77e946fd942146b51f80fc9a52ed700bba9a
Alex-135-135 <olecsij-andrijchuk@ukr.net>
2018-09-20 17:37:10 +0300

    TODO

--- app/components/TaskList.js
+++ app/components/TaskList.js
@@ -8,46 +8,41 @@ export default class TaskList extends Component{
 
   gettask = async () =>{
     let tasks = await AsyncStorage.getItem('task')
+    const i = await this.props.navigation.getParam('Task');
       tasks = JSON.parse(tasks)
       this.setState({tasks})
       console.log(tasks)
   }
-
-  componentDidMount(){
-    console.log('mount')
-     this.gettask()
+ componentWillMount(){
+    this.gettask()
   }
+
 
   render() {
     return (
       <View>
       <ScrollView style={{width: '100%'}}>
         <View>
+        <Button
+              title="Go to AddNewTask"
+              onPress={() => this.props.navigation.navigate('AddNewTask')}
+            />
           {this.state.tasks.map((a, b) =>
-            <View key={b}>
-              <View  style={{width: 300,  backgroundColor: '#e8f1fa',}}>
-                <Text>{a.task}</Text>
+            <View key={b} style={{width: '90%',  backgroundColor: 'red', left: '5%', borderBottomWidth: 2}}>
+              <View>
+                <Text>Task: {a.task}</Text>
               </View>
               <View>
-              <Picker
-                  selectedValue={a.priority}
-                  style={{ height: 50, width: 200,}}
-                  onValueChange={p => a.priority=p}>
-                  <Picker.Item label="LOW" value="LOW" />
-                  <Picker.Item label="MEDIUM" value="MEDIUM" />
-                  <Picker.Item label="CRITICAL" value="CRITICAL" />
-              </Picker>
               <Text >{a.date}</Text>
               </View>
-            </View>
-        )}
-          
-            <Button
-              title="Go to AddNewTask"
-              onPress={() => this.props.navigation.navigate('AddNewTask', {
-              Task: this.state,
+              <Button
+              title="Go to TaskView"
+              onPress={() => this.props.navigation.navigate('TaskView', {
+              key: b,
             })}
             />
+            </View>
+        )}
         </View>
             </ScrollView>
       </View>
--- app/components/TaskView.js
+++ app/components/TaskView.js
@@ -1,12 +1,97 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Picker, CheckBox, Button} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Picker, CheckBox, Button, AsyncStorage} from 'react-native';
 
 export default class TaskView extends Component{
+  state = {
+    task: '',
+    priority: 'LOW',
+    checked: false,
+    date: '',
+    tasks: [{}],
+    i: 0,
+  }
+
+  handleCreate = async () => {
+    if(this.state.task == ''){
+      alert('Заповніть поле Task')
+    }else{
+      alert('Task збережено')
+      this.state.tasks[this.state.i].task = this.state.task
+      this.state.tasks[this.state.i].priority = this.state.priority
+      this.state.tasks[this.state.i].checked = this.state.checked
+      this.state.tasks[this.state.i].date = this.state.date
+      AsyncStorage.setItem('task', JSON.stringify(this.state.tasks))
+    }
+  }
+  handleDelete = async () => {
+    this.state.tasks.splice(this.state.i, 1);
+    AsyncStorage.setItem('task', JSON.stringify(this.state.tasks))
+    this.props.navigation.navigate('TaskList')
+    alert('Task видалено')
+  }
+
+  gettask = async () =>{
+    let tasks = await AsyncStorage.getItem('task')
+    const i = await this.props.navigation.getParam('key');
+      tasks = JSON.parse(tasks)
+      //tasks[1]
+      this.setState(tasks[i])
+      this.setState({i})
+      this.setState({tasks})
+      console.log(this.state)
+  }
+
+  componentDidMount(){
+    this.gettask()
+  }
+
   render() {
     return (
-      <View>
-        <Text>Jane's profile</Text>
-      </View>
+       <View style={styles.container}>
+          <View>
+            <TextInput 
+              style={{width: 300}} 
+              multiline={true} 
+              value = {this.state.task}
+              onChangeText={(text) =>this.setState({task: text})}
+            />
+            <View style={{flexDirection: 'row',}}>
+              <Text style={{top: 15}}>Priority:</Text>
+              <Picker
+                selectedValue={this.state.priority}
+                style={{ height: 50, width: 200,}}
+                onValueChange={(itemValue, itemIndex) => this.setState({priority: itemValue})}>
+                <Picker.Item label="LOW" value="LOW" />
+                <Picker.Item label="MEDIUM" value="MEDIUM" />
+                <Picker.Item label="CRITICAL" value="CRITICAL" />
+              </Picker>
+            </View>
+             <View style={{flexDirection: 'row',}}>
+              <Text style={{top: 5}}>Complite: </Text>
+              <CheckBox
+              title='Click Here'
+              value={this.state.checked}
+              onChange={() => this.setState({checked: !this.state.checked})}
+              />
+            </View>
+          </View>
+          <View style={{width: 200, top: 25, flexDirection: 'row',}}>
+             <Button
+                title="Save"
+                onPress={this.handleCreate}
+              />
+              <Button
+                title="Delete"
+                onPress={this.handleDelete}
+              />
+          </View>
+           <View style={{width: 200, top: 40}}>
+            <Button
+              title="Go to AddNewTask"
+              onPress={() => this.props.navigation.navigate('TaskList')}
+            />
+          </View>
+        </View>
       
     );
   }
